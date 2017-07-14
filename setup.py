59ae4e83785aaebe783140462d9893f6366ed19d
Nikola Majcen <nikola.majcen@gmail.com>
2017-07-14 22:22:59 +0200

    Added project structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='mackup',
+    version='1.0',
+    description='Setup utility for Mac development environment',
+    author='Nikola Majcen',
+    url='https://nikolamajcen.com/',
+    packages=['mackup'],
+)
+
